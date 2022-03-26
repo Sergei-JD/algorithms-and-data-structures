b341b6b61f31798b860068777dca44d86a6eac9a
Sergei-JD <sergshlyazhko@gmail.com>
2022-03-26 21:36:47 +0400

    updated naive method for calculating fibonacci numbers (used memoization)

---
+++ src/com/aads/Main.java
@@ -0,0 +1,8 @@
+package com.aads;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
--- src/com/aads/fibonacci/FibonacciNumbers.java
+++ src/com/aads/fibonacci/FibonacciNumbers.java
@@ -1,17 +1,29 @@
 package com.aads.fibonacci;
 
+import java.util.Arrays;
+
 public class FibonacciNumbers {
 
     public static void main(String[] args) {
-        System.out.println(fibEffective(100));
+        int n = 100;
+        long[] mem = new long[n + 1];
+
+        Arrays.fill(mem, -1);
+
+        System.out.println(fibNaive(n, mem));
     }
 
-    //native (not effective)
-    private static long fibNative(int n) {
+    //naive (not effective)
+    private static long fibNaive(int n, long[] mem) {
+        if (mem[n] != -1)
+            return mem[n];
+
         if (n <= 1)
             return n;
+        long result = fibNaive(n - 1, mem) + fibNaive(n - 2, mem);
+        mem[n] = result;
 
-        return fibNative(n - 1) + fibNative(n - 2);
+        return result;
     }
 
     //effective
