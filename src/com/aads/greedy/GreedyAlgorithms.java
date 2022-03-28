f0412763fa30f69aebb0d759c5bd25c0b9a88afb
Sergei-JD <sergshlyazhko@gmail.com>
2022-03-28 17:04:06 +0400

    added Greedy Algorithms for optimal number of stops

--- src/com/aads/greedy/GreedyAlgorithms.java
+++ src/com/aads/greedy/GreedyAlgorithms.java
@@ -6,8 +6,11 @@ import java.util.Collections;
 public class GreedyAlgorithms {
 
     public static void main(String[] args) {
-        int[] digits = {3, 1, 7, 9, 9, 5};
-        System.out.println(maxNumberFromDigits(digits));
+//        int[] digits = {3, 1, 7, 9, 9, 5};
+//        System.out.println(maxNumberFromDigits(digits));
+
+        int[] stations = {0, 200, 375, 550, 750, 950};
+        System.out.println(minStops(stations, 400));
     }
 
     public static String maxNumberFromDigits(int[] digits) {
@@ -32,5 +35,29 @@ public class GreedyAlgorithms {
                 .sorted(Collections.reverseOrder())
                 .map(String::valueOf)
                 .toArray(String[]::new));
+    }
+
+    // returns -1 if it's impossible to get A to B
+    public static int minStops(int[] stations, int capacity) {
+        int result = 0; //  optimal number of stops
+        int currentStop = 0;
+
+        while (currentStop < stations.length - 1) {
+            int nextStop = currentStop;
+
+            while (nextStop < stations.length -1 &&
+                    stations[nextStop + 1] - stations[currentStop] <= capacity)
+                nextStop++;
+
+            if (currentStop == nextStop)
+                return -1;
+
+            if (nextStop < stations.length - 1)
+                result++;
+
+            currentStop = nextStop;
+        }
+
+        return result;
     }
 }
