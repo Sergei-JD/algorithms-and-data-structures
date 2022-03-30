d25c137a673d1c48eac03cd8bf14721a0f474c31
Sergei-JD <sergshlyazhko@gmail.com>
2022-03-30 17:00:34 +0400

    implemented the binary search algorithm

---
+++ src/com/aads/binarysaerch/BinarySearch.java
@@ -0,0 +1,28 @@
+package com.aads.binarysaerch;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(new int[]{-1, 3, 5, 8, 10, 15,16,20}, 16));
+    }
+
+    //  [1, 2, 3, 4, 5, 6, 7]
+    public static int binarySearch(int[] a, int key) {
+        int low = 0;
+        int high = a.length - 1;
+
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+
+            if (key < a[middle]) {
+                high = middle - 1;
+            } else if (key > a[middle]) {
+                low= middle + 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return - 1;
+    }
+}
