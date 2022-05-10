cd6f61a665f5b73ec2e8c6e7920ae1e6076f53b1
Sergei-JD <sergshlyazhko@gmail.com>
2022-05-10 21:00:10 +0400

    Search for the second smallest element in the array in one pass

---
+++ src/com/aads/sort/minTwoElementInArray/minTwoElementInArray.java
@@ -0,0 +1,28 @@
+package com.aads.sort.minTwoElementInArray;
+
+public class minTwoElementInArray {
+
+    public static void main(String[] args) {
+        int [] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+
+        int min = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min2 = min;
+                min = array[i];
+            } else if (array[i] < min2 && array[i] != min) {
+                min2 = array[i];
+            }
+        }
+
+        if (min2 != Integer.MAX_VALUE) {
+            System.out.println("Второй по итгтмальности элемент в массиве: " + min2);
+        } else {
+            System.out.println("Второго по минимальности элемента нет");
+        }
+
+        System.out.println("Минимальный элменет в массиве: " + min);
+    }
+}
