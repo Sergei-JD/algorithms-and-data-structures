245e8c72a93ae6296b761cc1faa50faa81eae8d0
Sergei-JD <sergshlyazhko@gmail.com>
2022-05-28 11:52:52 +0400

    implemented an algorithm for generating fractal noise

---
+++ src/com/aads/fractNoise/Utils.java
@@ -0,0 +1,37 @@
+package com.aads.fractNoise;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Utils {
+
+    public static void showImageWindow(Image image) {
+        showImageWindow(image, 1024, 768);
+    }
+
+    public static void showImageWindow(Image image, int width, int height) {
+        JFrame frame = new JFrame();
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel picLabel = new JLabel(new ImageIcon(image));
+
+        BorderLayout borderLayout = new BorderLayout();
+        frame.getContentPane().setLayout(borderLayout);
+        frame.getContentPane().add(picLabel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+
+    public static void measureTime(Runnable task, String taskName) {
+        long startTime = System.currentTimeMillis();
+        task.run();
+        long elapsed = System.currentTimeMillis() - startTime;
+        System.out.println(taskName + ": " + elapsed + " ms");
+    }
+
+    public static void assertTrue(boolean condition) {
+        if (!condition) {
+            throw new AssertionError();
+        }
+    }
+}
