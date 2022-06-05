7ada266e07ed54318647a1edfdf123b3f8e2863b
Sergei-JD <sergshlyazhko@gmail.com>
2022-06-05 13:45:14 +0400

    compared the performance of the comb algorithm with the bubble algorithm and the optimized bubble sorting algorithm by Knuth

---
+++ src/com/aads/sort/combSort/BubbleSort.java
@@ -0,0 +1,20 @@
+package com.aads.sort.combSort;
+
+public class BubbleSort {
+
+    public static void sort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+
+            for (int i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    int tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+}
