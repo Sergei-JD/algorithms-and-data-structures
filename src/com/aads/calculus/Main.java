4016911ccc3d39de5d1570ba231a8acc4f9ecb31
Sergei-JD <sergshlyazhko@gmail.com>
2022-06-11 18:19:53 +0400

    number systems: implemented the algorithm of translation into an arbitrary number system

---
+++ src/com/aads/calculus/Main.java
@@ -0,0 +1,36 @@
+package com.aads.calculus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getInRadix(255, 16));
+    }
+
+    public static String getInRadix(int number, int radix) {
+        List<Character> digits = getDigitTable();
+        if (radix < 2 || radix >= digits.size() || number < 0) {
+            throw new IllegalArgumentException();
+        }
+        StringBuilder valueStr = new StringBuilder();
+        while (number > 0) {
+            valueStr.insert(0, digits.get(number % radix));
+            number = number / radix;
+        }
+        return valueStr.toString();
+    }
+
+    private static List<Character> getDigitTable() {
+        List<Character> digits = new ArrayList<>();
+        for (char i = '0'; i <= '9'; i++) {
+            digits.add(i);
+        }
+        for (char i = 'A'; i <= 'Z'; i++) {
+            digits.add(i);
+        }
+        return digits;
+    }
+
+}
